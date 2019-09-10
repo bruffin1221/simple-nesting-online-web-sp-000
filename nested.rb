cb625f913b61aa5c2573753eb05c840b2735d6fc
Bruce Ruffin <bruffin1221@gmail.com>
2019-09-10 18:39:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 dennis_ritchie=programmer_hash[:dennis_ritchie][:languages]
+		 languages=programmer_hash[:dennis_ritchie][:languages]
 		 languages[1]="Assembly"
 end
 
